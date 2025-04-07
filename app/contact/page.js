705cec828171569fc076834e0d0d1bc61c3b87e8
ItsPrabhat0 <its.prabhat00@gmail.com>
2025-04-07 22:59:55 +0530

    added heading in remaning pages

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,9 +2,15 @@
 
 function Contact() {
   return (
-    <div className="h-[92vh] bg-gradient-to-b from-blue-100 to-red-100 text-black">contact page
+    <div className="h-[100vh] bg-gradient-to-b from-blue-100 to-red-100 text-black">
+    <h1 className="text-center p-2 text-2xl font-bold">CONTACT</h1>
+    <div className="flex  ">
 
     </div>
+    <div className="w-1/2  flex items-end justify-center">
+
+    </div>
+</div>
   )
 }
 
--- app/resume/page.js
+++ app/resume/page.js
@@ -2,10 +2,15 @@
 
 function Resume() {
     return (
-      <div className="h-[92vh] bg-gradient-to-b from-blue-100 to-red-100 text-black ">
-        Resume page
-  
+      <div className="h-[100vh] bg-gradient-to-b from-blue-100 to-red-100 text-black">
+      <h1 className="text-center p-2 text-2xl font-bold">RESUME</h1>
+      <div className="flex  ">
+
       </div>
+      <div className="w-1/2  flex items-end justify-center">
+
+      </div>
+  </div>
     )
   }
   
